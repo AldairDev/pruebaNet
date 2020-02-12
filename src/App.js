70bc43cf6e8dbf999dcd5d977ea311a4837f9017
aldair sosa <aldairsosa18@gmail.com>
2020-02-11 23:47:27 -0500

    some change1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deploying in netlify verify
+          Deploying in netlify verify 1
         </p>
         <a
           className="App-link"
